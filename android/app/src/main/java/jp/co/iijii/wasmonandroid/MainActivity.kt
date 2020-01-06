e4f416616848d303818d6819215d6c790c3f29b4
Yuji Yamamoto <yuji-yamamoto@iij.ad.jp>
2020-01-06 12:08:31 +0900

    Run wasm and copy sample wasm files in the background thread.

--- android/app/src/main/java/jp/co/iijii/wasmonandroid/MainActivity.kt
+++ android/app/src/main/java/jp/co/iijii/wasmonandroid/MainActivity.kt
@@ -3,34 +3,55 @@ package jp.co.iijii.wasmonandroid
 import android.os.Bundle
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import kotlinx.coroutines.*
 import java.io.File
 import java.io.FileOutputStream
 
 
-class MainActivity : AppCompatActivity() {
-    companion object
-    {
-        init { System.loadLibrary("greeting"); }
+class MainActivity : AppCompatActivity(), CoroutineScope {
+    companion object {
+        init {
+            System.loadLibrary("greeting"); }
     }
+
+    private val job = Job()
+    override val coroutineContext = Dispatchers.Main + job
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LogInit.init()
 
         setContentView(R.layout.activity_main)
-        val result = RustGreetings().sayHello("Android on Rust!")
-        findViewById<TextView>(R.id.greeting_view).text = result
 
         val gcdWasm = File(filesDir, "gcd.wasm")
 
         if (!gcdWasm.exists()) {
-            assets.open("gcd.wasm").use { src ->
-                FileOutputStream(gcdWasm).use { dest ->
-                    src.copyTo(dest)
+            findViewById<TextView>(R.id.initial_view).text =
+                resources.getString(R.string.copying_sample_wasm_files)
+            launch(Dispatchers.IO) {
+                assets.open("gcd.wasm").use { src ->
+                    FileOutputStream(gcdWasm).use { dest ->
+                        src.copyTo(dest)
+                    }
                 }
             }
         }
 
-        Wasmer.runWasm(gcdWasm.absolutePath)
+        findViewById<TextView>(R.id.initial_view).text =
+            resources.getString(R.string.running_wasm)
+
+        launch {
+            withContext(Dispatchers.IO) {
+                Wasmer.runWasm(gcdWasm.absolutePath)
+            }
+
+            // TODO: Show result of wasm
+            findViewById<TextView>(R.id.initial_view).text = "Finished"
+        }
+    }
+
+    override fun onDestroy() {
+        job.cancel()
+        super.onDestroy()
     }
 }
