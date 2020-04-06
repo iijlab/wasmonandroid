e638e00993b355a1592e78b5fcf2f4b48eee400e
YAMAMOTO Yuji <yuji-yamamoto@iij.ad.jp>
2020-04-06 19:15:29 +0900

    Choose the wasm file with a pull-down menu.

--- android/app/src/main/java/jp/co/iijii/wasmonandroid/MainActivity.kt
+++ android/app/src/main/java/jp/co/iijii/wasmonandroid/MainActivity.kt
@@ -1,13 +1,14 @@
 package jp.co.iijii.wasmonandroid
 
 import android.os.Bundle
-import android.widget.EditText
-import android.widget.TextView
+import android.view.View
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import jp.co.iijii.wasmonandroid.wasi.PreopenDirectory
 import jp.co.iijii.wasmonandroid.wasi.WasiEnv
 import jp.co.iijii.wasmonandroid.wasi.WasiFs
 import kotlinx.coroutines.*
+import kotlinx.coroutines.selects.select
 import java.io.File
 import java.io.FileOutputStream
 
@@ -22,48 +23,91 @@ class MainActivity : AppCompatActivity(), CoroutineScope {
     private val job = Job()
     override val coroutineContext = Dispatchers.Main + job
 
+    var selectedWasmFile: File? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         LogInit.init()
 
         setContentView(R.layout.activity_main)
 
-        val wasmFilePath = "wasi-hello.wasm"
-        val wasmFile = File(filesDir, wasmFilePath)
-
+        val wasmFiles = filesDir.listFiles()!!.toList().filter { it.name.endsWith(".wasm") }
         val statusView = findViewById<TextView>(R.id.status_view)
-        if (!wasmFile.exists()) {
-            statusView.text =
-                resources.getString(R.string.copying_sample_wasm_files)
-            launch(Dispatchers.IO) {
-                assets.open(wasmFilePath).use { src ->
-                    FileOutputStream(wasmFile).use { dest ->
-                        src.copyTo(dest)
+
+        wasmFiles.filter { !it.exists() }.also { nonExistingWasmFiles ->
+            if (nonExistingWasmFiles.isNotEmpty()) {
+                statusView.text =
+                    resources.getString(R.string.copying_sample_wasm_files)
+                launch(Dispatchers.IO) {
+                    nonExistingWasmFiles.forEach { wasmFile ->
+                        assets.open(wasmFile.name).use { src ->
+                            FileOutputStream(wasmFile).use { dest ->
+                                src.copyTo(dest)
+                            }
+                        }
                     }
                 }
             }
         }
 
-        statusView.text = resources.getString(R.string.running_wasm)
-        val consoleView = findViewById<EditText>(R.id.console_view)
-        launch {
-            withContext(Dispatchers.Main) {
-                val preopens = listOf(
-                    PreopenDirectory("test1", null, read = true, write = true, create = true),
-                    PreopenDirectory("test2", null, false, write = false, create = false)
-                )
-                Wasmer.runWasm(
-                    wasmFile.absolutePath,
-                    WasiEnv(
-                        WasiFs(
-                            stdin = NullHandle,
-                            stdout = OutTextViewHandle(consoleView),
-                            stderr = OutTextViewHandle(consoleView),
-                            preopens = preopens
+        findViewById<Spinner>(R.id.select_wasm_file).also { selectWasm ->
+            ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item,
+                wasmFiles.map { it.name }
+            ).also { adapter ->
+                adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+                selectWasm.adapter = adapter
+            }
+
+            selectWasm.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    selectedWasmFile = null
+                }
+
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    position: Int,
+                    id: Long
+                ) {
+                    selectedWasmFile = wasmFiles[position]
+                }
+            }
+        }
+
+        findViewById<Button>(R.id.run_wasm).also { button ->
+            button.setOnClickListener {
+                val wasmFile = selectedWasmFile ?: return@setOnClickListener
+
+                statusView.text = resources.getString(R.string.running_wasm)
+                val consoleView = findViewById<EditText>(R.id.console_view)
+                launch {
+                    withContext(Dispatchers.Main) {
+                        val preopens = listOf(
+                            PreopenDirectory(
+                                "test1",
+                                null,
+                                read = true,
+                                write = true,
+                                create = true
+                            ),
+                            PreopenDirectory("test2", null, false, write = false, create = false)
                         )
-                    )
-                )
-                statusView.text = resources.getString(R.string.finished_running_wasm)
+                        Wasmer.runWasm(
+                            wasmFile.absolutePath,
+                            WasiEnv(
+                                WasiFs(
+                                    stdin = NullHandle,
+                                    stdout = OutTextViewHandle(consoleView),
+                                    stderr = OutTextViewHandle(consoleView),
+                                    preopens = preopens
+                                )
+                            )
+                        )
+                        statusView.text = resources.getString(R.string.finished_running_wasm)
+                    }
+                }
             }
         }
     }
